89beda408aede2fc1f374d97b5cf722b0e26fea8
ravirathee <65222149+ravirathee@users.noreply.github.com>
2020-11-27 13:13:09 +0530

    renamed files

---
+++ Imp Que Ladder Level1/startingFile.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void test_case(){
+    
+}
+
+int main() {
+    //ifstream cin; ofstream cout; cin.open("input.txt"); cout.open("output.txt");
+    int t; cin>>t;
+    while(t--){
+        test_case();
+    }
+    return 0;
+}
